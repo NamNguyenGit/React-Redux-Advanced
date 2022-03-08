8e1119b2309bc7da0bb91c1ca24aa9567c45b9e7
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-03-08 17:57:55 +0700

    Practive React

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,50 @@
+import classes from "./CartItem.module.css";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
+
+const CartItem = (props) => {
+  const dispatch = useDispatch();
+
+  const { title, quantity, total, price, id } = props.item;
+
+  const removeToCartHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  const addToCartHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        price,
+        title,
+      })
+    );
+  };
+
+  return (
+    <li className={classes.item}>
+      <header>
+        <h3>{title}</h3>
+        <div className={classes.price}>
+          <span>$</span>
+          {total.toFixed(2)}
+          <span className={classes.itemprice}>
+            (<span>$</span>
+            {price.toFixed(2)}/item)
+          </span>
+        </div>
+      </header>
+      <div className={classes.details}>
+        <div className={classes.quantity}>
+          x <span>{quantity}</span>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={removeToCartHandler}>-</button>
+          <button onClick={addToCartHandler}>+</button>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
